dd6d84ed7d12bd5f9dff7c6dd4535787ff2aff68
Atle Nordland <atle.nordland@nav.no>
2023-01-16 11:15:52 +0100

    Ta i bruk ny dependencyløsning

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,11 +4,4 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://jitpack.io")
-}
-
-val dittNavDependenciesVersion = "2022.12.15-10.04-06c26ddff374"
-
-dependencies {
-    implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
 }
