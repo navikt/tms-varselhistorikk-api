0a16b25c58bf95b92ef687309cb6ab2fb8c05395
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-08-01 12:54:53 +0200

    oppdater lib-versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(Micrometer.registryPrometheus)
     implementation(Prometheus.logback)
     implementation("io.ktor:ktor-client-encoding:2.3.0")
-    implementation("com.github.navikt:tms-common-lib:1.0.1")
+    implementation("com.github.navikt:tms-common-lib:dc09781281")
 
     testImplementation(kotlin("test"))
     testImplementation(Kotest.assertionsCore)
