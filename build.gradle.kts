e77f481da57ebbdf7e1468b346047fa7e7db26fb
Christopher Santana <christopher.santana@nav.no>
2024-01-30 14:10:05 +0100

    Går vekk fra appdynamics baseimage i dockerfile.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,8 @@ kotlin {
 
 repositories {
     mavenCentral()
-    maven("https://maven.pkg.github.com/navikt/*") {
-        credentials {
-            username = System.getenv("GITHUB_ACTOR")?: "x-access-token"
-            password = System.getenv("GITHUB_TOKEN")?: project.findProperty("githubPassword") as String
-        }
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
     mavenLocal()
 }
